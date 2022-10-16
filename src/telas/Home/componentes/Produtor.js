83282e185d8d3669abbaa3092bb5d257f84ecb45
André Saxton <andre_saxton@hotmail.com>
2022-10-15 20:32:48 -0700

    Hook de redução

--- src/telas/Home/componentes/Produtor.js
+++ src/telas/Home/componentes/Produtor.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import { View, TouchableOpacity, Text, Image, StyleSheet } from 'react-native';
 import Estrelas from '../../../componentes/estrelas';
 
 export default function Produtor({ nome, imagem, distancia, estrelas }) {
-    const [ selecionado, setSelecionado ] = useState(false);
+    const [ selecionado, inverterSelecionado ] = useReducer(
+        (selecionado) => !selecionado,
+        false
+    );
 
     return <TouchableOpacity
             style={estilos.cartao}
-            onPress={() => setSelecionado(!selecionado)}
+            onPress={inverterSelecionado}
         >
         <Image style={estilos.imagem} source={imagem} accessibilityLabel={nome} />
         <View style={estilos.informacoes}>
